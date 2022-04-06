cf2501ab07cf405a17782fd364ac41cab4c1ab97
bekbolot00 <bekbolotzairbekov@gmail.com>
2022-04-06 15:10:20 +0300

    gfg

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let array = [100, 55, 15, 300, 20] 
+ 
+ 
+
+let i = array[0] + array[1]; 
+document.write(array[0]+ " + "  +  array[1] + " = " + i +"</br>"); 
+ 
+
+i = i + array[2]; 
+document.write((array[0] + array[1] ) + " + " + array[2] + " = " + i +"</br>"); 
+ 
+i = i +array[3]; 
+document.write((array[0] + array[1] + array[2]) + " + " + array[3] + " = " + i+"</br>");
+
